835079ffe0f03fe9d09e1a6456c871a0c8c49cc7
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-09-08 20:53:13 -0600

    commit tree a85b6fab1c03136398411dd410d9f94be76f262c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const num3 = 104
+const num4 = 25
+const mod = num3 % num4;
+
+const max = Math.max(0, 20)
